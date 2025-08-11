3a5554a59f6dd446ed237da2b9cca18213ee3c81
JOAO DUQUE NARDELLI WANDERMUREN <joao.dnwandermuren@senacsp.edu.br>
2025-08-11 20:34:22 -0300

    printf

--- AplicacaoVet/src/aplicacaovet/PrincipalVet.java
+++ AplicacaoVet/src/aplicacaovet/PrincipalVet.java
@@ -12,16 +12,16 @@ public class PrincipalVet {
         
         int vetor[] = new int[tamanho];
         
-        System.out.format("O tamanho do vetor é: %d\n", vetor.length);
+        System.out.printf("O tamanho do vetor é: %d\n", vetor.length);
         
         for(int i = 0; i < tamanho; i++) {
-            System.out.format("Qual será o valor do índice %d? ", i);
+            System.out.printf("Qual será o valor do índice %d? ", i);
             int valor = sc.nextInt();
             vetor[i] = valor;
         }
         
         for(int i = 0; i < tamanho; i++) {
-            System.out.format("Valor do índice %d: %d\n", i, vetor[i]);
+            System.out.printf("Valor do índice %d: %d\n", i, vetor[i]);
         }
         
     }
