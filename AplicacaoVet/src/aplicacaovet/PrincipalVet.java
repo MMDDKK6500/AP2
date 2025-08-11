037fa05d5b5dd82400b00456e1f2e217ceecb83c
JOAO DUQUE NARDELLI WANDERMUREN <joao.dnwandermuren@senacsp.edu.br>
2025-08-11 20:30:43 -0300

    Setar e Printar vetores

--- AplicacaoVet/src/aplicacaovet/PrincipalVet.java
+++ AplicacaoVet/src/aplicacaovet/PrincipalVet.java
@@ -1,9 +1,29 @@
 package aplicacaovet;
 
+import java.util.Scanner;
+
 public class PrincipalVet {
 
     public static void main(String[] args) {
-        int vetor[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Qual será o tamanho do vetor?");
+        int tamanho = sc.nextInt();
+        
+        int vetor[] = new int[tamanho];
+        
+        System.out.format("O tamanho do vetor é: %d\n", vetor.length);
+        
+        for(int i = 0; i < tamanho; i++) {
+            System.out.format("Qual será o valor do índice %d? ", i);
+            int valor = sc.nextInt();
+            vetor[i] = valor;
+        }
+        
+        for(int i = 0; i < tamanho; i++) {
+            System.out.format("Valor do índice %d: %d\n", i, vetor[i]);
+        }
+        
     }
     
 }
