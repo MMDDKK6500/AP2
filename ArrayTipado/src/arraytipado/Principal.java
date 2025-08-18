b5507c769cb83898eef2986f9b03d1b8347ce51a
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2025-08-18 20:25:26 -0300

    Dados inputaveis

--- ArrayTipado/src/arraytipado/Principal.java
+++ ArrayTipado/src/arraytipado/Principal.java
@@ -1,10 +1,15 @@
 package arraytipado;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
         Pessoa listaPessoas[] = new Pessoa[3];
+        
+        /*
         
         listaPessoas[0] = new Pessoa();
         listaPessoas[0].nome = "Zé Colmeia";
@@ -18,8 +23,23 @@ public class Principal {
         listaPessoas[1].telefone = "11 97776-5544";
         listaPessoas[1].idade = 37;
         
+        */
+
         for (int i = 0; i < listaPessoas.length; i++) {
-            System.out.printf("\n\tDados da %d pessoa \n", i+1);
+            System.out.printf("\n\tDados da %dº pessoa \n", i+1);
+            listaPessoas[i] = new Pessoa();
+            System.out.print("Nome: ");
+            listaPessoas[i].nome = sc.nextLine();
+            System.out.print("Email: ");
+            listaPessoas[i].email = sc.nextLine();
+            System.out.print("Telefone: ");
+            listaPessoas[i].telefone = sc.nextLine();
+            System.out.print("Idade: ");
+            listaPessoas[i].idade = sc.nextInt();
+        }
+        
+        for (int i = 0; i < listaPessoas.length; i++) {
+            System.out.printf("\n\tDados da %dº pessoa \n", i+1);
             System.out.println("\t" + listaPessoas[i].nome);
             System.out.println("\t" + listaPessoas[i].email);
             System.out.println("\t" + listaPessoas[i].telefone);
