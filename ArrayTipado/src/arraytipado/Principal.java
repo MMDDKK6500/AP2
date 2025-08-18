b4221b3a14f67442c0c3deb3861161de4285002e
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2025-08-18 20:02:06 -0300

    Nn copiar o ultimo..? pq?

--- ArrayTipado/src/arraytipado/Principal.java
+++ ArrayTipado/src/arraytipado/Principal.java
@@ -4,6 +4,29 @@ public class Principal {
 
     public static void main(String[] args) {
         
+        Pessoa listaPessoas[] = new Pessoa[3];
+        
+        listaPessoas[0] = new Pessoa();
+        listaPessoas[0].nome = "Zé Colmeia";
+        listaPessoas[0].email = "zecolmeia@gmail.com";
+        listaPessoas[0].telefone = "11 99976-4455";
+        listaPessoas[0].idade = 41;
+        
+        listaPessoas[1] = new Pessoa();
+        listaPessoas[1].nome = "Juca Bala";
+        listaPessoas[1].email = "juca_bala@gmail.com";
+        listaPessoas[1].telefone = "11 97776-5544";
+        listaPessoas[1].idade = 37;
+        
+        for (int i = 0; i < listaPessoas.length; i++) {
+            System.out.printf("\n\tDados da %d pessoa \n", i+1);
+            System.out.println("\t" + listaPessoas[i].nome);
+            System.out.println("\t" + listaPessoas[i].email);
+            System.out.println("\t" + listaPessoas[i].telefone);
+            System.out.println("\t" + listaPessoas[i].idade);
+            
+        }
+        
     }
     
 }
